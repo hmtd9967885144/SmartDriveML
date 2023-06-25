22c4522686984808228e0cf2ef093f175c0561b7
Hrishikesh Thakurdesai <hrishikesh.thakurdesai.92@gmail.com>
2023-06-25 12:15:35 +0530

    New Commit

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-#Test Codes
+import configparser
+
+config = configparser.ConfigParser()
+config.read_file(open(r'Config.txt'))
+path1 = config.get('My Section', 'path1')
+path2 = config.get('My Section', 'path2')
+path3 = config.get('My Section', 'path3')
+
+print(path1,path2,path3)
