481e62f763b0c94761a159a585a2646a41b6732a
Hrishikesh Thakurdesai <hrishikesh.thakurdesai.92@gmail.com>
2023-06-25 12:41:23 +0530

    New Commit

--- Test.py
+++ Test.py
@@ -1,5 +1,7 @@
 import configparser
 
+from tabulate import tabulate
+
 config = configparser.ConfigParser()
 config.read_file(open(r'Config.txt'))
 path1 = config.get('My Section', 'path1')
@@ -7,3 +9,20 @@ path2 = config.get('My Section', 'path2')
 path3 = config.get('My Section', 'path3')
 
 print(path1,path2,path3)
+
+
+
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("T1", style="dim", width=12)
+table.add_column("T1", style="dim", width=12)
+table.add_column("T1", style="dim", width=12)
+table.add_column("T1", style="dim", width=12)
+table.add_row("T1", "T1", "T1", "T1")
+table.add_row("T1", "T1", "T1", "T1")
+table.add_row("T1", "T1", "T1", "T1")
+console.print(table)
