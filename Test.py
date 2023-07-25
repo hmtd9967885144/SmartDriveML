c5b483facb04c49c3d308256eca3b09f665253d5
Hrishikesh Thakurdesai <hrishikesh.thakurdesai.92@gmail.com>
2023-07-25 19:34:29 +0530

    New Commit

--- Test.py
+++ Test.py
@@ -3,22 +3,26 @@ import collections
 collections.Callable = collections.abc.Callable
 from bs4 import BeautifulSoup
 import pandas as pd
+import tabulate
 from tabulate import tabulate
-
+pd.options.display.max_columns = None
 
 website_url = requests.get('https://learn.microsoft.com/en-us/azure/architecture/aws-professional/services').text
 soup = BeautifulSoup(website_url,'lxml')
-soup = BeautifulSoup(website_url, 'html.parser')
+#soup = BeautifulSoup(website_url, 'html.parser')
 
+count = 1
 for table in soup.find_all('table'):
-    # count = count+1
-    # if count==3:
-    text_file = open("Test_html.html.", "w")
-    text_file.write(str(table))
-    text_file.close()
-    tbl = pd.read_html('Test_html.html')[0]
-    print(tabulate(tbl, headers='keys', tablefmt='psql'))
-        #break
+    count = count+1
+    if count==3:
+        text_file = open("Test_html.html.", "w")
+        text_file.write(str(table))
+        text_file.close()
+        tbl = pd.read_html('Test_html.html')[0]
+        pdtabulate = lambda tbl: tabulate(tbl, headers='keys',tablefmt='psql')
+        print(pdtabulate(tbl))
+       # print(tabulate(tbl, headers='keys', tablefmt='psql', showindex=True))
+        break
 
 # tbl = pd.read_html('Test_html.html')[0]
 # print(tabulate(tbl, headers='keys', tablefmt='psql'))
---
+++ Test_Code_Parts.py
@@ -0,0 +1,3 @@
+from SmartDriveML import mlFunctions
+
+mlFunctions.DecisionTree()
